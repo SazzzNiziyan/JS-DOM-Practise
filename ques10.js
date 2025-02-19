0db23c19187df9267ce473c10c1e4068f2e00ebc
Sajal Singh <as3199432@gmail.com>
2025-02-19 23:37:20 +0530

    organized

--- ques10.js
+++
@@ -1,6 +0,0 @@
-var txtarea = document.querySelector("textarea");
-var count = document.querySelector("#counter");
-
-txtarea.addEventListener("input", function(){
-    count.textContent = txtarea.value.length;
-})
